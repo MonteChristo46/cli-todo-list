753942168aced59a52fba975726d59b6814839e5
MonteChristo46 <dhoeschele@outlook.com>
2022-06-27 20:13:06 +0200

    print for TodoList added

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,24 +1,64 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"github.com/MonteChristo46/cli-todo-list/pkg/todo"
+	"os"
 )
 
 const fileName string = "todoList.json"
 
+func handleError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 
 	// Defining the CLI input flags
-	add := flag.String("task", "", "Adds a task to TodoList")
+	add := flag.String("add", "", "Adds a task to TodoList")
 	complete := flag.String("complete", "", "Sets a task complete based on his name")
-	delete := flag.String("delete", "", "Deletes a task based on his name")
+	deleteTask := flag.String("delete", "", "Deletes a task based on his name")
+	list := flag.Bool("list", true, "Prints the TodoList in the command line")
+	creator := flag.String("creator", "Unkown", "The creator that will be listed when adding task")
+
+	// Parsing flags
+	flag.Parse()
 
 	// Initalizing TodoList
-	todoList := TodoList{}
+	todoList, err := todo.NewTodoList(fileName)
+	if err != nil {
+		fmt.Println(fileName + "not found")
+	}
 
-	switch {
-	case *add != "":
-		todoList.add(add, "Daniel")
-		return
+	// Logic for flag arguments
+	if *add != "" {
+		todoList.Add(*add, *creator)
+		err := todoList.StoreToFile(fileName)
+		handleError(err)
+	}
+	if *complete != "" {
+		err := todoList.CompleteTask(*complete)
+		handleError(err)
+		err = todoList.StoreToFile(fileName)
+		handleError(err)
+	}
+
+	if *deleteTask != "" {
+		todoList.Delete(*deleteTask)
+		err := todoList.StoreToFile(fileName)
+		handleError(err)
+	}
+	if *list {
+		todoList.Print()
+	}
+	if *creator != "" {
+		if *add != "" {
+			handleError(errors.New("creator can only be defined with valid task"))
+		}
 	}
 }
--- pkg/todo/todo.go
+++ pkg/todo/todo.go
@@ -4,89 +4,141 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/alexeyco/simpletable"
 	"io/ioutil"
+	"strconv"
 	"time"
 )
 
 type Item struct {
-	task           string
-	completed      bool
-	creationDate   time.Time
-	completionDate time.Time
-	creator        string
+	Task           string
+	Completed      bool
+	CreationDate   time.Time
+	CompletionDate time.Time
+	Creator        string
 }
 
 type TodoList struct {
-	items []Item
+	Items []Item
 }
 
-// taskExists check if a task already exists in TodoList
+func NewTodoList(filename string) (*TodoList, error) {
+	todoList := new(TodoList)
+	err := todoList.loadFile(filename)
+	return todoList, err
+}
+
+// taskExists check if a Task already exists in TodoList
 func (t TodoList) taskExists(task string) (bool, error) {
-	for i := range t.items {
-		if t.items[i].task == task {
+	for i := range t.Items {
+		if t.Items[i].Task == task {
 			return true, errors.New("Task already exists it can not be added")
 		}
 	}
 	return false, nil
 }
 
-// add appends an item to the TodoList
-func (t *TodoList) add(taskName string, creator string) {
+// Add appends an item to the TodoList
+func (t *TodoList) Add(taskName string, creator string) {
 
-	// Check if task already exists
+	// Check if Task already exists
 	_, err := t.taskExists(taskName)
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		item := Item{
-			task:           taskName,
-			completed:      false,
-			creationDate:   time.Now(),
-			completionDate: time.Time{},
-			creator:        creator,
+			Task:           taskName,
+			Completed:      false,
+			CreationDate:   time.Now(),
+			CompletionDate: time.Time{},
+			Creator:        creator,
 		}
-		t.items = append(t.items, item)
+		t.Items = append(t.Items, item)
 	}
 }
 
-// delete removes an element from the task list
-func (t *TodoList) delete(task string) {
+// Delete removes an element from the Task list
+func (t *TodoList) Delete(task string) {
 	var list []Item
-	for i := range t.items {
-		item := t.items[i]
-		if item.task != task {
-			t.items = append(list, item)
+	for i := range t.Items {
+		item := t.Items[i]
+		if item.Task != task {
+			t.Items = append(list, item)
 		}
 	}
-	t.items = list
+	t.Items = list
 }
 
-// completeTask set a task to completed
-func (t *TodoList) completeTask(task string) {
-	for i := range t.items {
-		if t.items[i].task == task {
-			t.items[i].completed = true
-			t.items[i].completionDate = time.Now()
+// CompleteTask set a Task to completed
+func (t *TodoList) CompleteTask(task string) error {
+	for i := range t.Items {
+		if t.Items[i].Task == task {
+			if t.Items[i].Completed != true {
+				t.Items[i].Completed = true
+				t.Items[i].CompletionDate = time.Now()
+				return nil
+			} else {
+				return errors.New("task already set to complete")
+			}
 		}
 	}
+	return errors.New("nothing there to complete")
 }
 
 // loadFile reads a json file containing the tasks
-func (t *TodoList) loadFile(fileName string) {
+func (t *TodoList) loadFile(fileName string) error {
 	// Reading file as binary
 	file, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	// Load the binary from the file into TodoList object
 	err = json.Unmarshal(file, t)
+	return err
 }
 
-// storeToFile saves TodoList to json file
-func (t TodoList) storeToFile(filename string) error {
+// StoreToFile saves TodoList to json file
+func (t *TodoList) StoreToFile(filename string) error {
 	data, err := json.Marshal(t)
 	if err != nil {
 		return err
 	}
-	return ioutil.WriteFile(filename, data, 064)
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+func (t TodoList) Print() {
+	table := simpletable.New()
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "TASK"},
+			{Align: simpletable.AlignCenter, Text: "COMPLETED"},
+			{Align: simpletable.AlignCenter, Text: "CREATION DATE"},
+			{Align: simpletable.AlignCenter, Text: "COMPLETION DATE"},
+			{Align: simpletable.AlignCenter, Text: "CREATOR"},
+		},
+	}
+
+	for i := range t.Items {
+
+		//
+		var completedText string
+		if t.Items[i].Completed {
+			completedText = "✅"
+		} else {
+			completedText = "❌"
+		}
+
+		row := []*simpletable.Cell{
+			{Text: strconv.Itoa(i + 1)},
+			{Text: t.Items[i].Task, Align: simpletable.AlignCenter},
+			{Text: completedText, Align: simpletable.AlignCenter},
+			{Text: t.Items[i].CompletionDate.Format("2006-01-02 15:04")},
+			{Text: t.Items[i].CreationDate.Format("2006-01-02 15:04")},
+			{Text: t.Items[i].Creator},
+		}
+		table.Body.Cells = append(table.Body.Cells, row)
+	}
+
+	fmt.Println(table.String())
 }
